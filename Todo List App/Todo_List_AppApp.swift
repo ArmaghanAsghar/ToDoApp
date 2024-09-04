6c71fe216f2dd1272f025105b55cce772ed536de
armaghanasghar <armaghanwa@gmail.com>
2024-09-03 18:34:36 -0700

    Initial Commit

---
+++ Todo List App/Todo_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_AppApp.swift
+//  Todo List App
+//
+//  Created by Armaghan Asghar on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
